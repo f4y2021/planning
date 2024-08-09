f41992e0f82afc742db9e64967ae9cfc267e2f18
f4y2021 <93331814+f4y2021@users.noreply.github.com>
2024-08-09 15:52:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -157,7 +157,9 @@ def main(file_objs, sheet_name, description):
 
 # Streamlit UI
 #st.title("Task Data Processor")
-st.columns(3)[1,2,1].image('logo_400.png')
+
+colu1, colu2, colu3 = st.columns([1, 3, 1])
+colu2.image('logo_400.png')
 
 file_objs = st.file_uploader("Upload Excel Files from Template", type=["xlsx"], accept_multiple_files=True)
 
